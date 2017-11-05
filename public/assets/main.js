384f7ef355d80719d13801d6484e6674da909486
chris <chrischocko@gmail.com>
2017-11-04 23:47:00 -0400

    qualify based on global var data

--- public/assets/main.js
+++ public/assets/main.js
@@ -13,20 +13,17 @@ function ViewModel() {
 function QualificationModel() {
   var $scope = this;
 
-  // variables
   $scope.homeOwner = ko.observable(true);
+  $scope.ageBased = ko.observable(true);
   $scope.overSixty = ko.observable(false);
   $scope.household = ko.observable(1);
   $scope.income = ko.observable(20000);
+  $scope.qualified = ko.observable(true);
 
-  // show info
   $scope.incomeText = ko.computed(function() {
     return $scope.income() + ' yr';
   });
 
-  // check qualifications
-  $scope.homeRepairs = ko.observable(true);
-  $scope.otherServices = ko.observable(true);
 
   var checkQualifications = ko.computed(function() {
     if (!$scope.homeOwner()) {
@@ -35,39 +32,26 @@ function QualificationModel() {
       return;
     }
 
-    // other services for all ages
-    // are the same as over 60 home repairs
-
-    var matrix = [
-      [0, 0],
-      // < 60, > 60  | household
-      [23250, 37150],// 1
-      [26550, 42450],// 2
-      [29850, 47750],// 3
-      [33150, 53050],// 4
-      [35850, 57300],// 5
-      [38500, 61550],// 6
-      [41150, 65800],// 7
-      [43800, 70050],// 8
-      [46450, 74250],// 9
-      [49100, 78500]// 10
-    ];
-
     var household = $scope.household();
-    var income = $scope.income();
+    INCOME_LIMITS.forEach(function(householdLimit) {
+      if (householdLimit['Household'] == household) {
 
-    var limit = matrix[household][1];
-    var qualified = income <= limit;
+        var limit;
+        if ($scope.ageBased()) {
+          if ($scope.overSixty()) {
+            limit = householdLimit['Over 60'];
+          } else {
+            limit = householdLimit['Under 60'];
+          }
+        } else {
+          limit = householdLimit['All ages'];
+        }
 
-    $scope.otherServices(qualified);
+        var qualified = $scope.income() <= limit;
+        $scope.qualified(qualified);
 
-    if ($scope.overSixty()) {
-      $scope.homeRepairs(qualified);
-    } else {
-      limit = matrix[household][0];
-      qualified = income <= limit;
-      $scope.homeRepairs(qualified);
-    }
+      }
+    });
   });
 
 }
